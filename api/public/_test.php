b703d6d19cf423f1e437601c1f2ca181c4749704
Kamil Kozaczyński <kozaczynski.kamil@gmail.com>
2023-11-30 01:07:30 +0100

    Setup behat tests (#13)
    * Setup behat tests
    
    * Fix formating
    
    * Add generating JWT keypair to CI pipeline

---
+++ api/public/_test.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+
+$_SERVER['APP_ENV']   = 'test';
+$_SERVER['APP_DEBUG'] = true;
+
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+Debug::enable();
+
+return function () {
+    return new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+};
